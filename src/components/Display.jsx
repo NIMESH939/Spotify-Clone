fe1a0478592628bcdaaa62f9e915dfdb02b1aa9f
NIMESH939 <nimeshkavindaofficial@gmail.com>
2024-10-17 22:33:47 +0530

    album item bgColor

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -11,9 +11,13 @@ const Display = () => {
   const albumId = isAlbum ? location.pathname.slice(-1) : "";
   const bgColor = albumsData[Number(albumId)].bgColor;
 
-  useEffect(()=>{
-    
-  })
+  useEffect(() => {
+    if (isAlbum) {
+      displayRef.current.style.background = `linear-gradient(${bgColor},#121212)`;
+    } else {
+      displayRef.current.style.background = `#121212`;
+    }
+  });
 
   return (
     <div className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0">
@@ -21,6 +25,7 @@ const Display = () => {
         <Route path="/" element={<DisplayHome />} />
         <Route path="/album/:id" element={<DisplayAlbum />} />
       </Routes>
+      s
     </div>
   );
 };
